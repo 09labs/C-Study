a0691dbf615b3f9a56db9f42411054655dce6623
09labs <09labs@gmail.com>
2021-01-15 15:42:25 +0900

    add char array to vector

---
+++ char_array_to_vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+	std::vector<uint8_t> v;
+	uint8_t array[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+	v.assign(std::begin(array), std::begin(array) + 4);
+
+	for(int i = 0 ; i < v.size() ; i++)
+		printf("%d\n", v[i]);
+
+	return 0;
+}
